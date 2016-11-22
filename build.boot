addbf961563f9f2f1ec8747525f085be09f85fbb
David Orme <dorme@bradsdeals.com>
2016-11-22 10:51:22 -0600

    WIP: Basic structure

--- build.boot
+++ build.boot
@@ -1,111 +1,47 @@
-;; Generic build.boot for lein projects
-;;
-;; * Configures itself from Leiningen's project.clj
-;; * Enables auto-test on file save with `boot testing'
-;; * Enables nightlight web editor and repl during auto-test task
+(set-env! :project "com.github.coconutpalm.boot-leiningen" :version "0.0.1"
 
-;; Build environment
-(set-env! :repositories #(conj % '["jitpack" {:url "https://jitpack.io"}])
+          :source-paths #{"src" "test"}
 
-          :dependencies '[[org.clojure/clojure         "1.8.0"     :scope "provided"]
-                          [org.clojure/clojurescript   "1.9.229"   :scope "provided" :classifier "aot"]
-                          [seancorfield/boot-new       "0.4.7"] ; FIXME: Need a :scope here?
-                          [boot/core                   "2.6.0"     :scope "provided"]
-                          [adzerk/bootlaces            "0.1.13"    :scope "test"]
-                          [adzerk/boot-test            "1.1.0"     :scope "test"]
-                          [funcool/boot-codeina        "0.1.0-SNAPSHOT" :scope "test"]
-                          [adzerk/boot-cljs            "1.7.228-1" :scope "test"]
-                          [pandeiro/boot-http          "0.7.3"     :scope "test"]
-                          [adzerk/boot-reload          "0.4.12"    :scope "test"]
-                          [adzerk/boot-cljs-repl       "0.3.3"     :scope "test"]
-                          [com.cemerick/piggieback     "0.2.1"     :scope "test"]
-                          [weasel                      "0.7.0"     :scope "test"]
-                          [org.clojure/tools.nrepl     "0.2.12"    :scope "test"]
-                          [jonase/eastwood             "0.2.3"     :scope "test"]
-                          [tolitius/boot-check         "0.1.3"     :scope "test"]
-                          [spyscope                    "0.1.6"     :scope "test"]
-                          [samestep/boot-refresh       "0.1.0"     :scope "test"]
-                          [metosin/boot-alt-test       "0.1.2"     :scope "test"]
-                          [adzerk/boot-test            "1.1.0"     :scope "test"]
-                          [adzerk/boot-jar2bin         "1.1.0"     :scope "test"]
-                          [nightlight                  "1.0.0"     :scope "test"]])
+          :repositories #(conj % '["jitpack" {:url "https://jitpack.io"}])
 
+          :dependencies '[[org.clojure/clojure         "1.8.0"]
+                          [org.clojure/clojurescript   "1.9.293"]
 
-(require '[clojure.string :as str]
-         '[adzerk.boot-test :refer :all]
-         '[metosin.boot-alt-test :refer [alt-test]]
-         '[adzerk.boot-reload :refer [reload]]
-         '[adzerk.boot-jar2bin :refer :all]
-         '[adzerk.boot-cljs-repl :refer [cljs-repl start-repl]]
-         '[bultitude.core :as b]
-         '[tolitius.boot-check :as check]
-         '[spyscope.core :refer :all]
-         '[samestep.boot-refresh :refer [refresh]]
-         '[funcool.boot-codeina :refer :all]
-         '[clojure.pprint :refer [pprint]]
-         '[nightlight.boot :refer [night]])
+                          [boot/base                   "2.6.0"]
+                          [boot/core                   "2.6.0"]
+
+                          [adzerk/bootlaces            "0.1.13"]
+                          [adzerk/boot-test            "1.1.2"]
+                          [adzerk/boot-cljs            "1.7.228-1"]
+                          [adzerk/boot-reload          "0.4.12"]
+                          [adzerk/boot-cljs-repl       "0.3.3"]
+
+                          [boot/pod                    "2.6.0"]
+                          [boot/aether                 "2.6.0"]
+                          [boot/worker                 "2.6.0"]
+
+                          [pandeiro/boot-http          "0.7.3"]
+                          [funcool/boot-codeina        "0.1.0-SNAPSHOT"]
+                          [seancorfield/boot-new       "0.4.7"]
+                          [com.cemerick/piggieback     "0.2.1"]
+                          [weasel                      "0.7.0"]
+                          [org.clojure/tools.nrepl     "0.2.12"]
+                          [jonase/eastwood             "0.2.3"]
+                          [tolitius/boot-check         "0.1.3"]
+                          [spyscope                    "0.1.6"]
+                          [samestep/boot-refresh       "0.1.0"]
+                          [metosin/boot-alt-test       "0.1.2"]
+                          [adzerk/boot-test            "1.1.0"]
+                          [adzerk/boot-jar2bin         "1.1.0"]
+                          [nightlight                  "1.2.1"]])
 
 
-(deftask repls
-  "From-Lein: Start an nrepl server with watch/refresh support."
-  []
-  (comp
-   (repl :server true)
-   (watch)
-   (refresh)
-   (notify :visual :audible)))
+(require '[boot-leiningen.core :refer :all]
+         '[funcool.boot-codeina :refer :all])
 
 
-(deftask replc
-  "Run an nrepl client."
-  []
-  (repl :client true))
-
-
-(deftask lint
-  "From-Lein: Reveal lint in the codebase."
-  []
-  (comp
-   (check/with-yagni)
-   (check/with-eastwood)
-   (check/with-kibit)
-   (check/with-bikeshed)))
-
-
-(deftask print-boot-deps
-  "A simple task to print the dependencies that Boot sees.  Must be
-  composed with (from-lein) in order to print dependencies slurped
-  in from project.clj."
-  []
-  (pprint (get-env :dependencies))
-  identity)
-
-
-(deftask dev
-  "Watch the file system and run tests on the current project.  Starts
-  an nrepl server and a nightlight editor for interaction with the running
-  application.  Automatically reloads namespaces and re-runs tests when files
-  change."
-  []
-  (comp
-   (repl :server true)
-   (night "--port" "0")
-   (watch)
-   (refresh)
-   (test)
-   (speak)
-   (notify :visual :audible)))
-
-
-(deftask test+install
-  "Run tests, build a jar, and install it into the local repo."
-  []
-  (comp
-   (test)
-   (speak)
-   (notify :visual :audible)
-   (pom)
-   (jar)
-   (install)))
-
-
+(task-options!
+     apidoc {:title       "Boot-Leiningen"
+             :version     (get-env :version)
+             :sources     #{"src" "test"}
+             :description "Boot and Leiningen utilities with a focus on Leiningen interop from Boot."})
--- src/boot_leiningen/core.clj
+++ src/boot_leiningen/core.clj
@@ -1,88 +1,134 @@
 (ns boot-leiningen.core
+  (:refer-clojure :exclude [test])
+  (:require [boot.core :refer :all]
+            [boot.pod :as pod]
+            [boot.task.built-in :refer :all]
+            [adzerk.boot-test :refer :all]
+            [adzerk.boot-reload :refer [reload]]
+
+            [clojure.java.io :as io]
+            [clojure.repl :as repl]
+            [clojure.test :as t]
+
+            [clojure.string :as str]
+            [adzerk.boot-jar2bin :refer :all]
+            [adzerk.boot-cljs-repl :refer [cljs-repl start-repl]]
+            [bultitude.core :as b]
+            [tolitius.boot-check :as check]
+            [spyscope.core :refer :all]
+            [samestep.boot-refresh :refer [refresh]]
+            [funcool.boot-codeina :refer :all]
+            [clojure.pprint :refer [pprint]]
+            [nightlight.boot :refer [night]])
   (:import [java.io PushbackReader ByteArrayInputStream InputStreamReader]))
 
 
+
+(defn inject-env
+  "Used by build.boot to inject the environment into this namespace."
+  [env-fn] (env-fn))
+
+
+(defn doc-str
+  "Return the doc string for symbol"
+  [symbol]
+  (with-out-str (repl/doc symbol)))
+
+
 (defn string-reader
+  "Returns a PushbackReader containing the specified string"
   [s]
   (-> s (.getBytes) (ByteArrayInputStream.) (InputStreamReader.) (PushbackReader.)))
 
 
-(defn forms
-  "Return a lazy seq of unevaluated forms in the specified PushbackReader."
-  [reader]
-  (let [form (read {:eof ::eof} reader)]
-    (if (= ::eof form)
-      nil
-      (lazy-seq (cons form (forms reader))))))
-
-
-(defn project-clj
-  "Return the (defproject) form from a project.clj file.  Evals all forms it
-  finds before the defproject form in the file so that any constants defined
-  earlier will be available for the defproject form."
-  []
-  (loop [project-forms (-> "project.clj" slurp string-reader forms)]
-    (let [form (first project-forms)]
-      (cond
-        (and (seq? form)
-             (= "defproject" (str (first form)))) form
-        (= nil form)                              (throw (IllegalStateException. "No defproject form found."))
-        :else                                     (do (eval form)
-                                                      (recur (rest project-forms)))))))
-
-
 (defmacro unq
-  "If a token needs to be unquoted, resolve it's value."
+  "If a token needs to be unquoted, resolve its value, else return the token itself."
   [val] `~val)
 
 
-;; Build environment
-(set-env! :repositories #(conj % '["jitpack" {:url "https://jitpack.io"}])
+(t/with-test
 
-          :dependencies '[[org.clojure/clojure         "1.8.0"     :scope "provided"]
-                          [org.clojure/clojurescript   "1.9.229"   :scope "provided" :classifier "aot"]
-                          [seancorfield/boot-new       "0.4.7"] ; FIXME: Need a :scope here?
-                          [boot/core                   "2.6.0"     :scope "provided"]
-                          [adzerk/bootlaces            "0.1.13"    :scope "test"]
-                          [adzerk/boot-test            "1.1.0"     :scope "test"]
-                          [funcool/boot-codeina        "0.1.0-SNAPSHOT" :scope "test"]
-                          [adzerk/boot-cljs            "1.7.228-1" :scope "test"]
-                          [pandeiro/boot-http          "0.7.3"     :scope "test"]
-                          [adzerk/boot-reload          "0.4.12"    :scope "test"]
-                          [adzerk/boot-cljs-repl       "0.3.3"     :scope "test"]
-                          [com.cemerick/piggieback     "0.2.1"     :scope "test"]
-                          [weasel                      "0.7.0"     :scope "test"]
-                          [org.clojure/tools.nrepl     "0.2.12"    :scope "test"]
-                          [jonase/eastwood             "0.2.3"     :scope "test"]
-                          [tolitius/boot-check         "0.1.3"     :scope "test"]
-                          [spyscope                    "0.1.6"     :scope "test"]
-                          [samestep/boot-refresh       "0.1.0"     :scope "test"]
-                          [metosin/boot-alt-test       "0.1.2"     :scope "test"]
-                          [adzerk/boot-test            "1.1.0"     :scope "test"]
-                          [adzerk/boot-jar2bin         "1.1.0"     :scope "test"]
-                          [nightlight                  "1.0.0"     :scope "test"]])
+  (defn forms
+    "Return a lazy seq of unevaluated forms in the specified PushbackReader."
+    [reader]
+    (let [form (read {:eof ::eof} reader)]
+      (if (= ::eof form)
+        nil
+        (lazy-seq (cons form (forms reader))))))
+
+  (t/testing "Whitespace returns an empty seq"
+    (t/is (empty? (forms (string-reader "")))))
+
+  (t/testing "A comment returns an empty seq"
+    (t/is (empty? (forms (string-reader ";Just a comment")))))
+
+  (t/testing "One form"
+    (t/is (= 1 (count (forms (string-reader "(def i 1)"))))))
+
+  (t/testing "Two forms"
+    (let [fs (forms (string-reader "(def i 1) (+ i 3)"))]
+      (t/is (= 2 (count fs)))
+      (t/is (= 4 (do (eval (first fs)) (eval (second fs))))))))
 
 
-(require '[clojure.string :as str]
-         '[adzerk.boot-test :refer :all]
-         '[metosin.boot-alt-test :refer [alt-test]]
-         '[adzerk.boot-reload :refer [reload]]
-         '[adzerk.boot-jar2bin :refer :all]
-         '[adzerk.boot-cljs-repl :refer [cljs-repl start-repl]]
-         '[bultitude.core :as b]
-         '[tolitius.boot-check :as check]
-         '[spyscope.core :refer :all]
-         '[samestep.boot-refresh :refer [refresh]]
-         '[funcool.boot-codeina :refer :all]
-         '[clojure.pprint :refer [pprint]]
-         '[nightlight.boot :refer [night]])
+(defn- test-project [] (str (System/getProperty "user.dir") "/resources/project.clj"))
+(defn- get-var [v] (some-> (ns-resolve *ns* 'version) var-get))
+
+
+(t/with-test
+    (defn project-clj
+      "Return the (defproject) form from a project.clj file.  Evals all forms it
+      finds before the defproject form in the file so that any constants defined
+      earlier will be available for the defproject form."
+      ([p]
+       (loop [project-forms (-> p slurp string-reader forms)]
+         (let [form (first project-forms)]
+           (cond
+             (and (seq? form)
+                  (= "defproject" (str (first form)))) form
+             (= nil form)                              (throw (IllegalStateException. "No defproject form found."))
+             :else                                     (do (eval form)
+                                                           (recur (rest project-forms)))))))
+      ([] (project-clj "project.clj")))
+
+    (let [prj (project-clj (test-project))]
+      (t/testing "Evaluates forms before the (defproject) form"
+        (t/is (= "1.8.0" (get-var 'version))))
+
+      (t/testing "Returns the defproject form as a seq"
+        (t/is (seq? prj))
+        (t/is (= "defproject" (str (first prj))))))
+
+    (t/testing "Throws IllegalStateException if defproject cannot be found."
+      (t/is (thrown? IllegalStateException (project-clj (string-reader "(def i 3)"))))))
+
+
+(t/with-test
+  (defn- lein-proj
+    "Return Leiningen metadata map from a project.clj file."
+    [project-file]
+    (let [project (project-clj project-file)]
+      (merge  {:project (str (second project))
+               :version (nth project 2)}
+              (->> project
+                   (drop 3)
+                   (partition 2)
+                   (map (fn [[k v]] [k (unq v)]))
+                   (into {})))))
+
+  (let [result-map (lein-proj (test-project))]
+    (t/testing "Parses the project name and version"
+      (t/is (= "com.github.shopsmart/clj-foundation" (:project result-map)))
+      (t/is (= "0.9.16" (:version result-map))))
+
+    (t/testing "Parses the defproject body into metadata map"
+      (t/is (= "https://github.com/shopsmart/clj-foundation" (:url result-map))))))
 
 
 (deftask from-lein
-  "Take this project's metadata from a Leiningen project.clj."
-  []
-
-  (let [project   (project-clj)
+  "Merge a Leiningen project.clj file's metadata into this project's boot environment."
+  [p project VAL str "The Leiningen project.clj file to load.  Defaults to project.clj in the current directory."]
+  (let [project   (project-clj (:project *opts* "project.clj"))
         lein-proj (merge {:project (str/replace (str (second project)) "/" ".")
                           :version (nth project 2)}
                          (->> project
@@ -117,102 +163,169 @@
      jar    (conj {}
                   (when (:main lein-proj)     [:main (:main lein-proj)])
                   (if   (:jar-name lein-proj) [:file (:jar-name lein-proj)]
-                                              [:file (str (:project lein-proj) ".jar")]))
+                        [:file (str (:project lein-proj) ".jar")]))
      bin    (conj {:output-dir "bin"}
                   (when (:jvm-opts lein-proj) [:jvm-opt (:jvm-opts lein-proj)]))
      apidoc {:title       (:project lein-proj)
              :version     (:version lein-proj)
-             :sources     (get-env :source-paths)
+             :sources     (or (:source-paths lein-proj) #{"src" "test"})
              :description (:description lein-proj "")}
      pom    {:project     (symbol (:project lein-proj))
              :version     (:version lein-proj)
              :description (:description lein-proj "")
              :url         (:url lein-proj "")
              :scm         (:scm lein-proj "")
-             :license     (get lein-proj :license {"EPL" "http://www.eclipse.org/legal/epl-v10.html"})}))
+             :license     (get lein-proj :license {"EPL" "http://www.eclipse.org/legal/epl-v10.html"})})
+   identity))
+
+
+
+
+
+(deftask check-conflicts
+  "Verify there are no dependency conflicts."
+  []
+  (with-pass-thru fs
+    (require '[boot.pedantic :as pedant])
+    (let [dep-conflicts (resolve 'pedant/dep-conflicts)]
+      (if-let [conflicts (not-empty (dep-conflicts pod/env))]
+        (throw (ex-info (str "Unresolved dependency conflicts. "
+                             "Use :exclusions to resolve them!")
+                        conflicts))
+        (println "\nVerified there are no dependency conflicts."))))
   identity)
 
 
+#_(deftask l-check-conflicts
+   "Like check-conflicts, but obtains project metadata from project.clj"
+   (comp
+    (from-lein)
+    (check-conflicts)))
+
+
 (deftask repls
-  "From-Lein: Start an nrepl server with watch/refresh support."
+  "Start an nrepl server with watch/refresh support."
   []
   (comp
-   (from-lein)
    (repl :server true)
    (watch)
    (refresh)
-   (notify :visual :audible)))
+   (notify :visual true :audible true)))
+
+
+(deftask l-repls
+  "Like repls, but obtains project metadata from project.clj"
+  []
+  (comp
+   (from-lein)
+   (repls)))
 
 
 (deftask replc
   "Run an nrepl client."
   []
-  (repl :client true))
+  (comp
+   (repl :client true)
+   (watch)
+   (refresh)
+   (speak)
+   (notify :visual true :audible true)))
+
+
+(deftask l-replc
+  "Like replc, but obtains project metadata from project.clj"
+  []
+  (comp
+    (from-lein)
+    (replc)))
 
 
 (deftask lint
-  "From-Lein: Reveal lint in the codebase."
+  "Reveal lint in the codebase."
   []
   (comp
-   (from-lein)
    (check/with-yagni)
    (check/with-eastwood)
    (check/with-kibit)
    (check/with-bikeshed)))
 
 
-(deftask docs
-  "From-Lein: Generate API docs"
+(deftask l-lint
+  "Like lint, but obtains project metadata from project.clj"
   []
-  (from-lein)
-  (apidoc))
+  (comp
+   (from-lein)
+   (lint)))
 
 
-(deftask print-boot-deps
-  "A simple task to print the dependencies that Boot sees.  Must be
-  composed with (from-lein) in order to print dependencies slurped
-  in from project.clj."
+(deftask print-deps
+  "Print the project dependencies that Boot sees."
   []
   (pprint (get-env :dependencies))
   identity)
 
 
-(deftask print-deps
-  "From-Lein: Print dependencies, including those slurped in from project.clj"
+(deftask l-print-deps
+  "Like print-deps, but obtains project metadata from project.clj"
   []
   (comp
    (from-lein)
-   (print-boot-deps)))
+   (print-deps)))
 
 
 (deftask testing
-  "From-Lein: Watch the file system and run tests on the current project.  Starts
+  "Watch the file system and run tests on the current project.  Starts
   an nrepl server and a nightlight editor for interaction with the running
-  application.  Automatically reloads namespaces and re-runs tests when files
-  change."
+  application.  Automatically reloads namespaces, re-runs tests, and
+  generates apidoc when files change."
   []
   (comp
-   (from-lein)
    (repl :server true)
    (night "--port" "0")
    (watch)
    (refresh)
-   (alt-test)  ; Only run changed and dependent tests
+   (test)
    (speak)
-   (notify :visual :audible)))
+   (notify :visual true :audible true)
+   (apidoc)))
+
+
+(deftask l-testing
+  "Like testing, but obtains project metadata from project.clj"
+  []
+  (comp
+    (from-lein)
+    (testing)))
 
 
 (deftask test+install
-  "From-Lein: Run tests, build a jar, and install it into the local repo."
+  "Run tests, build a jar, and install it into the local repo."
+  []
+  (comp
+   (test)
+   (speak)
+   (notify :visual true :audible true)
+   (pom)
+   (jar)
+   (speak)
+   (notify :visual true :audible true)
+   (install)))
+
+
+(deftask l-test+install
+  "Like test+install, but obtains project metadata from project.clj"
   []
   (comp
    (from-lein)
-   (test)
-   (speak)
-   (notify :visual :audible)
-   (pom)
-   (jar)
-   (install)))
+   (test+install)))
+
+
+(deftask l-apidoc
+  "Like apidoc, but obtains project metadata from project.clj"
+  []
+  (comp
+   (from-lein)
+   (apidoc)))
 
 
 (deftask uberbin
@@ -228,3 +341,14 @@
    (uber)
    (jar)
    (bin)))
+
+
+(deftask l-uberbin
+  "Like uberbin, but obtains project metadata from project.clj"
+  []
+  (comp
+   (from-lein)
+   (uberbin)))
+
+
+(t/run-tests)
